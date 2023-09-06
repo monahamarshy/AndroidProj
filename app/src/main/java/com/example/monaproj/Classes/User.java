26a917c35596a5a29fa523a8c3c88168c2c9142a
MonaHamarshy <mona.nezar2006@gmail.com>
2023-09-06 14:24:43 +0300

    user

---
+++ app/src/main/java/com/example/monaproj/Classes/User.java
@@ -0,0 +1,15 @@
+package com.example.monaproj.Classes;
+
+public class User {
+ private String Email;
+    private String password;
+
+    public  User(String Email,String password){
+        this.Email=Email;
+        this.password=password;
+
+    }
+
+
+
+}
